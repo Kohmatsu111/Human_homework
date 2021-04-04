d54411eba2e900410b35185d3cbb62ea119fdd6f
kohmatsu111 <voy.al.cabo@gmail.com>
2021-04-04 12:00:43 +0900

    正直、よくわかりません。何が違うのか・・。

--- app/src/main/java/jp/techacademy/masahiro/kohmatsu/human/Human.kt
+++ app/src/main/java/jp/techacademy/masahiro/kohmatsu/human/Human.kt
@@ -1,19 +1,26 @@
 package jp.techacademy.masahiro.kohmatsu.human
 
+import android.icu.text.StringSearch
 import android.util.Log
+// Animalクラスを継承したHumanクラスを宣言
+    class Human : Animal{
+        //引数付きコンストラクタをAnimalクラスから継承
+        constructor(name: String, age: Int) : super(name, age)
+//Human クラス内で、hobby を定義
+    var hobby: String
+        //引数付きコンストラクタ
+        constructor(hobby: String) {
+            this.hobby = hobby
+        }
 
-open class Human: Animal,Thinkable{
-    //引数付きコンストラクタ
-    constructor(name: String, age: Int, hobby: String):super(name,age,hobby){
-    }
+        // Animalクラスのメソッドをオーバーライド
+        override fun say() {
+            Log.d("human", "私の名前は" + this.name + "です。 年は" + this.age + "歳です。")
 
-    // Animalクラスのメソッドをオーバーライド
-    override fun say() {
-        Log.d("kotlintest", "私の名前は" + this.name + "です。 年は" + this.age +"歳です。")
-    }
+            // Thinkable クラスのメソッドをHumanクラスに実装
+            fun think() {
+                Log.d("human", "私は" + this.hobby + "について考える。")
 
-    // Thinkableインターフェイスのメソッドをオーバーライド
-    override fun think(){
-        Log.d("kotlintest",  "私は" + this.hobby + "について考える")
+            }
+        }
     }
-}
--- app/src/main/java/jp/techacademy/masahiro/kohmatsu/human/MainActivity.kt
+++ app/src/main/java/jp/techacademy/masahiro/kohmatsu/human/MainActivity.kt
@@ -2,16 +2,21 @@ package jp.techacademy.masahiro.kohmatsu.human
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 名前を「幸松正浩」、年齢「43」歳、趣味「自転車」のHumanインスタンスを作る
-        val human = Human("幸松正浩",43,"自転車")
+        // 名前を「幸松正浩」、「年齢43歳」のHumanインスタンスを作る
+        val human = Human("幸松正浩",43, "自転車")
 
-        human.say()
-        human.think()
+        human.say()//　自己紹介文
+        Log.d("human", "私の名前は" + human.name + "です。 年は" + human.age +"歳です。")
+
+        human.think()// 趣味に関して
+        Log.d("human", "私は" + human.hobby )
+
     }
 }
